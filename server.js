9669658a889059b1ced88a004ace75bfe6f8ace1
AA <aalborgil002@gmail.com>
2024-08-02 17:14:28 -0400

    Added comments on how to complete back-end step by step (in server.js)

--- server.js
+++ server.js
@@ -9,3 +9,18 @@ sequelize.sync().then(() => {
         console.log(`Server running on port ${PORT}`)
     });
 });
+
+
+// Step by step notes
+
+// 1. Create package.json file & download started npm packages (will download more as needed along the way)
+// 2. Create Server (server.js file)
+// 3. Create Database (schema.sql file in db folder)
+// 4. Connect Database to server (connection.js in config folder)
+// 5. Secure DB credentials (.env file)
+// 6. Create DB tables (models folder)
+
+// To do
+
+// Continue in video minute 1:37:30
+// Create Models 
